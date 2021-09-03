b320cc4cf1098553bad377b833d0004eef081e83
Joel Beckum <joelbeckum@gmail.com>
2021-09-03 15:15:45 -0500

    Refactored HumanPlayer class

--- HumanPlayer.cs
+++ HumanPlayer.cs
@@ -1,10 +1,34 @@
+using System;
+
 namespace ShootingDice
 {
     // TODO: Complete this class
 
     // A player the prompts the user to enter a number for a roll
-    public class HumanPlayer
+    public class HumanPlayer : Player
     {
-
+        public override int Roll()
+        {
+            int response = 0;
+            while(response == 0)
+            {
+                try
+                {
+                    Console.WriteLine($"Pick a number between 1 and {DiceSize}: ");
+                    response = int.Parse(Console.ReadLine());
+                    if (response > DiceSize)
+                    {
+                        Console.WriteLine($"I'm sorry, you must pick a number between 1 and {DiceSize}");
+                        response = 0;
+                    }
+                }
+                catch(FormatException)
+                {
+                    Console.WriteLine($"I'm sorry, you must input a number between 1 and {DiceSize}");
+                }
+            }
+            
+            return response;
+        }
     }
 }
